ccb9376cc744c23cfe87c1c94ed4ba52258feff4
Undery <127064386+Undery33@users.noreply.github.com>
2023-05-20 13:40:52 +0900

    Add files via upload

---
+++ BR31.py
@@ -0,0 +1,73 @@
+import random as rand
+user = 0    # Get User Number
+default = 1 # Sum Game Number of last
+WIN = 30    # BR31 rules one: who enter 31 is defeat
+
+# Set Error 1: Over or Under enter Number
+class ErrorNum(Exception):
+    def __str__(ern):
+        return("Wrong Number")
+    
+# Set Error 2: Variable is not type Integer
+class WrongEnter(Exception):
+    def __str__(wen):
+        return ("Please Enter Number")
+    
+# Start Game (__main__)
+print("Welcome to BR31 game!")
+while True:
+    # Check and get User Number
+    try:
+        user = input("Input Number : ")
+        user_num = user.split(" ")
+        # if user enter wrong Number
+        if len(user_num) > 3 or int(user_num[-1]) <= default or int(user_num[-1]) > default + 3:
+            raise ErrorNum()
+    except ErrorNum as e:
+        print(e)
+        continue
+    except ValueError:
+        print(WrongEnter())
+        continue 
+
+    # Save last of User number
+    default = int(user_num[-1])
+
+    # Check Game rules by User
+    if default >= 31:
+        print("Over Number 31")
+        print("You Lose!")
+        break
+
+    # Computer set Number
+    Com_Rand_Set = rand.randint(1, 3)
+    # Can win Computer (1)
+    if default <= 26 and default >= 24:
+        Com_Rand_Set = 1
+    if default == 27:
+        Com_Rand_Set = 3
+    Com_def = Com_Rand_Set + default
+
+    # Can win Computer (2)
+    if Com_def == 27:
+        Com_Rand_Set = 1
+
+    # Check Game rules by Computer
+    if Com_def >= 31 or default == WIN:
+        print("Computer Over Number 31")
+        print("You Win!")
+        break
+
+    # Print Computer Number
+    if Com_def <= WIN:
+        print("Computer Number: ", end='')
+        for cmn in range(Com_Rand_Set):
+            print((cmn + 1) + default, end=' ')
+            # Can win Computer (3)
+            if (cmn + 1) + default == WIN:
+                Com_def = WIN
+                break
+    print(" ")
+
+    # Save last of Computer Number
+    default = Com_def
